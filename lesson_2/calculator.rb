9fa183b062cf77d5446ab3743e8c9b611316b0d1
SamWegs <samwegleitner@gmail.com>
2020-02-07 15:56:45 -0700

    Adding updates

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -4,21 +4,37 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def valid_number?(num)
-  num.to_i().is_a(Numeric) # fix this line
+def valid_integer?(num)
+  num.to_i().to_s() == num
+end
+
+def valid_float?(num)
+  num.to_f().to_s() == num
+end
+
+def integer_or_float(num)
+  if num.include? '.'
+    num.to_f()
+  else
+    num.to_i()
+  end
+end
+
+def valid?(num)
+  valid_integer?(num) || valid_float?(num)
 end
 
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+  word_op = case op
+            when '1'
+              'Adding'
+            when '2'
+              'Subtracting'
+            when '3'
+              'Multiplying'
+            when '4'
+              'Dividing'
+            end
 end
 
 prompt("Welcome to Calculator! Enter your name:")
@@ -42,7 +58,8 @@ loop do # main loop
     prompt("What's the first number?")
     number1 = Kernel.gets().chomp()
 
-    if valid_number?(number1)
+    if valid?(number1)
+      number1 = integer_or_float(number1)
       break
     else
       prompt("Hmm.. that doesn't look like a valid number")
@@ -54,7 +71,8 @@ loop do # main loop
     prompt("What's the second number?")
     number2 = Kernel.gets().chomp()
 
-    if valid_number?(number2)
+    if valid?(number2)
+      number2 = integer_or_float(number2)
       break
     else
       prompt("Hmm.. that doesn't look like a valid number")
@@ -82,17 +100,17 @@ loop do # main loop
     end
   end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
+  prompt("#{operation_to_message(operator)} #{number1} and #{number2}...")
 
   result = case operator
            when '1'
-             number1.to_i() + number2.to_i()
+             number1 + number2
            when '2'
-             number1.to_i() - number2.to_i()
+             number1 - number2
            when '3'
-             number1.to_i() * number2.to_i()
+             number1 * number2
            when '4'
-             number1.to_f() / number2.to_f()
+             number1.to_f() / number2.to_f() #since division leave the float
            end
 
   prompt("The result is #{result}")
