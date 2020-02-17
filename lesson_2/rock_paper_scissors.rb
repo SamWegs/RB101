cf2a5620648ba6a6fb6539c46dbd48ff5405c022
SamWegs <samwegleitner@gmail.com>
2020-02-17 09:25:05 -0700

    Adding rock paper scissors updates and additional questions pertaining this problem.

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,45 +1,53 @@
 VALID_CHOICES = ['rock', 'paper', 'scissors']
 
-def prompt(message)
-    Kernel.puts("=> #{message}")
-end
-
 def display_results(player, computer)
-    if (player == 'rock' && computer == 'scissors') ||
-        (player == 'paper' && computer == 'rock') ||
-        (player == 'scissors' && computer == 'paper')
-        prompt("You won!")
-    elsif (player == 'rock' && computer == 'paper') ||
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+    ("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
         (player == 'rock' && computer == 'scissors') ||
         (player == 'scissors' && computer == 'rock')
-        prompt("Computer won!")
-    else
-        prompt("It's a tie!")
-    end
+    ("Computer won!")
+  else
+    ("It's a tie!")
+  end
+end
+
+=begin
+def test_method
+  prompt('Test message')
+end
+
+test_method
+=end
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
 loop do
-    choice = ''
-    loop do
-        prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-        choice = Kernel.gets().chomp()
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
 
-        if VALID_CHOICES.include?(choice)
-            break
-        else
-            prompt("That's not a valid choice.")
-        end
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
     end
+  end
 
-    computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.sample
 
-    Kernel.puts("You chose: #{choice}. Computer chose: #{computer_choice}.")
+  Kernel.puts("You chose: #{choice}. Computer chose: #{computer_choice}.")
 
-    display_results(choice, computer_choice)
+  prompt(display_results(choice, computer_choice))
 
-    prompt("Do you want to play again?")
-    answer = Kernel.gets().chomp()
-    break unless answer.downcase().start_with?('y')    
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
 
 prompt("Thank you for playing. Good bye!")
