a6f9c25f5f8341d3b6389c229bc9f1fa30d2ad42
SamWegs <samwegleitner@gmail.com>
2020-02-18 15:16:17 -0700

    Added short conversion for names as well as start of scoreboard

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,11 +1,27 @@
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+VALID_CHOICES_SHORT = ['r', 'p', 'sc', 'l', 'sp']
+
+def convert_abrv_name(input)
+  result = case input
+          when 'r'
+            'rock'
+          when 'p'
+            'paper'
+          when 'sc'
+            'scissors'
+          when 'l'
+            'lizard'
+          when 'sp'
+            'spock'
+          end
+end
 
 def win?(first, second)
   (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
-    (first == 'paper' && (second == 'rock' ||second == 'spock')) ||
-    (first == 'scissors' && (second == 'paper' || second == 'lizard')) ||
-    (first == 'lizard' && (second == 'spock' || second == 'paper')) ||
-    (first == 'spock' && (second == 'scissors' || second == 'rock'))
+  (first == 'paper' && (second == 'rock' ||second == 'spock')) ||
+  (first == 'scissors' && (second == 'paper' || second == 'lizard')) ||
+  (first == 'lizard' && (second == 'spock' || second == 'paper')) ||
+  (first == 'spock' && (second == 'scissors' || second == 'rock'))
 end
 
 def prompt(message)
@@ -22,24 +38,49 @@ def display_results(player, computer)
   end
 end
 
+def score_board(player, computer)
+  if win?(player, computer)
+    user_score += 1
+    ("User Score: #{user_score} | Computer Score: #{computer_score}")
+  elsif win?(computer, player)
+    computer_score += 1
+    ("User Score: #{user_score} | Computer Score: #{computer_score}")
+  else
+    ("No points awarded")
+  end
+end
+
+
+
 loop do
   choice = ''
+  user_score = 0
+  computer_score = 0
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("or choose: #{VALID_CHOICES_SHORT.join(', ')}")
     choice = Kernel.gets().chomp()
+    
+    
 
-    if VALID_CHOICES.include?(choice)
-      break
+    if VALID_CHOICES.include?(choice) || VALID_CHOICES_SHORT.include?(choice)
+        break
     else
       prompt("That's not a valid choice.")
     end
   end
 
+  choice = convert_abrv_name(choice)
+
   computer_choice = VALID_CHOICES.sample
 
-  Kernel.puts("You chose: #{choice}. Computer chose: #{computer_choice}.")
+  Kernel.puts("You chose: #{choice.upcase()}. Computer chose: #{computer_choice.upcase()}.")
 
   prompt(display_results(choice, computer_choice))
+
+ 
+
+  prompt(score_board(choice, computer_choice))
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
