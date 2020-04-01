29be505f739032e98cdb67841512a6f7778ac550
Sam Wegleitner <samwegleitner@gmail.com>
2020-04-01 17:34:40 -0600

    Easy exercises in lesson 3

---
+++ lesson_3/easy_1_6.rb
@@ -0,0 +1,6 @@
+famous_words = "seven years ago..."
+#famous_words.insert(0, 'Four score and ')
+famous_words.prepend('Four score and ')
+
+puts famous_words
+p famous_words
---
+++ lesson_3/easy_1_8.rb
@@ -0,0 +1,6 @@
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3,
+   "BamBam" => 4, "Pebbles" => 5 }
+
+dad = flintstones.assoc("Barney") # assoc returns a key value pair
+
+p dad
