4509f7c94aa3aef8ec4d60ef6b0ba239a457822b
Sam Wegleitner <samwegleitner@gmail.com>
2020-03-31 20:48:29 -0600

    Adding easy 4 & 5

--- lesson_3/easy_1_3.rb
+++ lesson_3/easy_1_3.rb
@@ -1,2 +1,2 @@
 advice = "Few things in life are as important as house training your pet dinosaur."
-p advice
+p advice.sub! "important", "urgent" # could also use gsub! (same operation)
---
+++ lesson_3/easy_1_5.rb
@@ -0,0 +1,2 @@
+puts (10..100).to_a.include?(42)
+puts (10..100).cover?(42)
