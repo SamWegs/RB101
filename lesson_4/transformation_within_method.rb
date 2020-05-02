77f4eedb1f9007a8e9e7c60aeb6b5805bd5594db
Sam Wegleitner <samwegleitner@gmail.com>
2020-05-02 00:50:49 -0600

    Adding transformation_indicies.rb

--- lesson_4/transformation_within_method.rb
+++ lesson_4/transformation_within_method.rb
@@ -1,15 +1,37 @@
 def double_numbers(numbers)
-	doubled_numbers = []
-	counter = 0
+  doubled_numbers = []
+  counter = 0
 
-	loop do
-		break if counter == numbers.size
+  loop do
+    break if counter == numbers.size
 
-		current_number = numbers[counter]
-		doubled_numbers << current_number * 2
+    current_number = numbers[counter]
+    doubled_numbers << current_number * 2
 
-		counter += 1
-	end
+    counter += 1
+  end
 
-	doubled_numbers		
+  doubled_numbers
 end
+
+def doubled_numbers!(numbers)
+  counter = 0
+
+  loop do
+    break if counter == numbers.size
+
+    current_number = numbers[counter]
+    numbers[counter] = current_number * 2
+
+    counter += 1
+  end
+
+  numbers
+end
+
+
+my_numbers = [1, 4, 2, 7, 2, 6]
+p my_numbers
+p double_numbers(my_numbers)
+p doubled_numbers!(my_numbers)
+p my_numbers
